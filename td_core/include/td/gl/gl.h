0264d62f9fd0dcfc05146f0fe113322d23798262
Vyacheslav Mayorov <vyacheslav.mayorov@gmail.com>
2020-06-13 18:22:22 +0300

    GLEW static linking

--- td_core/include/td/gl/gl.h
+++ td_core/include/td/gl/gl.h
@@ -1,14 +1,17 @@
-//
-// Created by acc15 on 03.04.19.
-//
-
 #pragma once
 
 #if defined(__ANDROID__)
-#   include <GLES2/gl2.h>
+
+    #include <GLES2/gl2.h>
+
 #elif defined(__linux__) || defined(WIN32)
-#   include <GL/glew.h>
+
+    #define GLEW_STATIC
+    #include <GL/glew.h>
+
 #else
-#   error "Your platform isn't supported at the moment"
+
+    #error "Your platform isn't supported at the moment"
+
 #endif
 
