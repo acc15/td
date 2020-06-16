da16a92c2d3429c6896d798343fa89b4fcb6c1df
Vyacheslav Mayorov <vyacheslav.mayorov@gmail.com>
2020-06-16 23:16:28 +0300

    Added gl debug

--- td_core/include/td/gl/debug.h
+++ td_core/include/td/gl/debug.h
@@ -5,7 +5,7 @@
 
 namespace td {
 
-bool is_gl_debug_enabled(const std::vector<std::string> args);
+bool is_gl_debug_enabled(const std::vector<std::string>& args);
 
 void enable_gl_debug();
 void disable_gl_debug();
---
+++ td_core/include/td/gl/enum.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <unordered_map>
+#include "gl.h"
+
+namespace td {
+
+    const char* gl_enum_string(GLenum v, const char* default_value = "UNKNOWN");
+
+}
+
